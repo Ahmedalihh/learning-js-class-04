5e199e9c02452bd9b9bf900737d75ef7158707e7
Ahmedalihh <ahmedalibbb227@gmail.com>
2024-12-17 23:18:56 +0500

    learn prompt

---
+++ app.js
@@ -0,0 +1,96 @@
+// console.log("hello class");
+
+// var a = 10;
+// a = a + 1;
+// a += 1;
+//increment
+// a++;
+// a++;
+//decrement
+
+// a--
+// console.log( "a", a);
+
+// var fullname = "ahmedali";
+// fullname += "hh";
+// console.log("fullname", fullname);
+
+// a++ postincrement
+// ++a preincrement
+// var a = 20;
+// ++a;
+// console.log(a++, a);
+// console.log(++a, a);
+
+// console.log(a++);
+
+//++a pehle add pher print
+//a++ pehle print pher add
+
+// var a = 50;
+// console.log(a++,a)
+// console.log(++a,a)
+
+// var a = 20; //19 //18
+// var result = a++ + a + ++a
+// var result = ++a + ++a +a + a++ + a++;
+// var result = a++ - ++a + a + a-- + --a + a;
+// var result = --a + --a + a-- + a + a++ + ++a - --a + a++ - --a;
+//  19 + 18 + 18 + 17 + 17 + 18 - 17 + 17 + 18 - 17
+// console.log(result);
+
+// var a = 8; // 9  // 10 // 9
+// var result = a++ + ++a + --a - a--;
+//    8 + 10 + 9 - 9 =18
+// console.log(result);
+
+// var result = a-- - a-- - ++a + a++ + a + --a;
+//   8 - 7 - 7 + 7 + 7 + 6 = 16
+// console.log(result);
+
+// var result = a++ + a++ + ++a - --a - ++a + a++ + --a;
+// 8+9 +11 -10 = 29
+
+// console.log(result);
+
+// prompt
+
+// prompt("enter your name");
+// prompt("enter your name");
+// var username = prompt("enter your name");
+// console.log(username);
+// console.log(username);
+
+// var num1 = +prompt("enter your num1");
+// var num2 = +prompt("enter your num2");
+
+// console.log(num1 + num2);
+// var username=prompt("enter your email" , "john doe@gmail.com")
+// console.log(username)
+
+//condition if / else
+
+
+
+// = ==> assigning opretor
+// == ==> equal to
+
+
+
+
+var age = 25;
+if ((age == 18)) {
+  console.log("allow");
+}else {
+    console.log("not allow")
+}
+
+// if (condition ) {      //block of statement
+
+// }
+
+//else {
+// }
+
+// console.log("allow");
+// console.log("not allow");
